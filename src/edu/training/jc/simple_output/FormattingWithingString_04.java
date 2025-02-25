8847b0cb15881167269aa3525e6adb37b92ad2ff
YauhenFrolenkov <esfrolenkov@icloud.com>
2025-02-25 20:05:10 +0300

    Add files via upload

---
+++ src/edu/training/jc/simple_output/FormattingWithingString_04.java
@@ -0,0 +1,12 @@
+package edu.training.jc.simple_output;
+
+public class FormattingWithingString_04 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("1) Todoist - Balancing power and simplicity.\n"
+				+ "2) TickTick - Embedded calendars and simplicity.\n"
+				+ "3) Microsoft To Do - Microsoft power users.");
+	}
+
+}
---
+++ src/edu/training/jc/simple_output/FrameOutput_03.java
@@ -0,0 +1,15 @@
+package edu.training.jc.simple_output;
+
+public class FrameOutput_03 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("***********");
+		System.out.println("*    *    *");
+		System.out.println("*    *    *");
+		System.out.println("*    *    *");
+		System.out.println("***********");
+		
+	}
+
+}
